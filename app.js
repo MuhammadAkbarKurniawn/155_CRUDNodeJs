80947ef150c67dc78a6845bc35c8af82b030323a
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-06 17:13:59 +0700

    STYLING_PAWD_155

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const session = require('express-session');
 const authRoutes = require('./routes/authRoutes');
 const { isAuthenticated } = require('./middlewares/middleware.js');
 
-
+app.use('/public',express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(expressLayouts);
